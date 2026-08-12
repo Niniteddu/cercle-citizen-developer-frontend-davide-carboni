1f9f706c72b8f311cafa939a388b744e583fa73d
CarboniDavide <31761218+CarboniDavide@users.noreply.github.com>
2026-08-12 13:44:16 +0200

    clean: move routing in a new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
-import ObjectivesPage from './pages/ObjectivesPage'
-import ProcessPage from './pages/ProcessPage'
-import WelcomePage from './pages/WelcomePage'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/objectifs" element={<ObjectivesPage />} />
-          <Route path="/demarche" element={<ProcessPage />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
